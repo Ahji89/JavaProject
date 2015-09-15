662149a7f991101d6ded08c3b7ceb18630f00cb0
Ahji89 <a54039@nate.com>
2015-09-15 17:29:28 +0900

    1회차 숙제 보강 및 2회차 기본 폴더 생성

---
+++ 01Class/src/homework/SeHee_Kim/MyTV.java
@@ -0,0 +1,8 @@
+package homework.SeHee_Kim;
+
+public class MyTV extends TV{
+	MyTV(){
+		statusList.add(new TVStatus("¹à±â", 0, 0, 100, 2));
+		statusList.add(new TVStatus("¸í¾Ï", 0, 0, 100, 2));
+	}
+}
